63535fee10aac04a085812369f8327d68ae41541
Saimon Alam <ibnshafique@gmail.com>
2022-06-13 01:54:25 +0600

    create common app

---
+++ server/common/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """
+    It's a abstract model for created_at and modified_at field
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
